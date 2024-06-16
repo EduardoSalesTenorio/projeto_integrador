d8a733321d33b12ec44a73a7628c49de0d232cd8
Eduardo Sales <eduardosallis30@gmail.com>
2024-06-16 20:55:04 -0300

    Correção banco Tela Exercicios
    Correções. O banco esta chamado normalmente.

--- lib/telas/exercicios.dart
+++ lib/telas/exercicios.dart
@@ -17,8 +17,9 @@ class Exercicios extends StatefulWidget {
 }
 
 class _ExerciciosState extends State<Exercicios> {
+  String perguntaBancoDados = "Eduardo_Sales_Tenorio";
   String respostaCerta = "";
-  List<String> perguntas = [];
+  List<String> pergunta = [];
   String respostaUsuario = "_______";
   List<bool> visibilidadeBotoes = [];
   int cont = 0;
@@ -32,8 +33,17 @@ class _ExerciciosState extends State<Exercicios> {
   @override
   void initState() {
     super.initState();
-    cont = widget.numeroExercicio - 1;
-    _carregarPalavras();
+
+    cont = widget.numeroExercicio;
+
+    _carregarDados();
+  }
+
+  Future<void> _carregarDados() async {
+    await _carregarPalavras();
+    await _carregarCaminhaImagem();
+    await _carregarCaminhoVideo();
+    _carregarQuestao();
   }
 
   Future<void> _carregarPalavras() async {
@@ -43,9 +53,6 @@ class _ExerciciosState extends State<Exercicios> {
       setState(() {
         palavras = listaPalavras.map((palavra) => palavra.palavra).toList();
         isLoading = false;
-        if (palavras.isNotEmpty) {
-          _carregarCaminhaImagem();
-        }
       });
     } catch (e) {
       print('Erro ao carregar palavras: $e');
@@ -63,9 +70,6 @@ class _ExerciciosState extends State<Exercicios> {
         caminhaImagem =
             listaPalavras.map((palavra) => palavra.palavra).toList();
         isLoading = false;
-        if (caminhaImagem.isNotEmpty) {
-          _carregarCaminhoVideo();
-        }
       });
     } catch (e) {
       print('Erro ao carregar palavras: $e');
@@ -82,9 +86,6 @@ class _ExerciciosState extends State<Exercicios> {
       setState(() {
         caminhoVideo = listaPalavras.map((palavra) => palavra.palavra).toList();
         isLoading = false;
-        if (caminhoVideo.isNotEmpty) {
-          _carregarQuestao();
-        }
       });
     } catch (e) {
       print('Erro ao carregar palavras: $e');
@@ -96,10 +97,15 @@ class _ExerciciosState extends State<Exercicios> {
 
   void _carregarQuestao() {
     if (palavras.isNotEmpty) {
-      perguntas = palavras[cont].split("_");
-      perguntas.shuffle();
-      respostaCerta = palavras[cont].replaceAll("_", "");
-      visibilidadeBotoes = List.filled(perguntas.length, true);
+      pergunta = palavras[0].split("_");
+
+      pergunta.shuffle();
+
+      respostaCerta = palavras[0].replaceAll("_", "");
+
+      visibilidadeBotoes = List.filled(pergunta.length, true);
+    } else {
+      print("Nenhuma palavra carregada");
     }
   }
 
@@ -107,13 +113,11 @@ class _ExerciciosState extends State<Exercicios> {
     String resposta = respostaUsuario.replaceAll(" ", "");
     if (resposta == respostaCerta) {
       // Lógica quando a resposta está correta
-
-      cont++;
     } else {
       // Lógica quando a resposta está incorreta
-
       _resetarQuestao();
     }
+    cont++;
   }
 
   void _resposta(String texto) {
@@ -122,7 +126,7 @@ class _ExerciciosState extends State<Exercicios> {
         respostaUsuario = texto;
       } else {
         respostaUsuario =
-            '$respostaUsuario$texto'; // Adiciona um espaço entre as palavras
+            '$respostaUsuario $texto'; // Adiciona um espaço entre as palavras
       }
     });
   }
@@ -130,9 +134,9 @@ class _ExerciciosState extends State<Exercicios> {
   void _resetarQuestao() {
     setState(() {
       respostaUsuario = "_______";
-      perguntas.shuffle();
-      respostaCerta = palavras[cont % palavras.length].replaceAll("_", "");
-      visibilidadeBotoes = List.filled(perguntas.length, true);
+      pergunta.shuffle();
+      respostaCerta = perguntaBancoDados.replaceAll("_", "");
+      visibilidadeBotoes = List.filled(pergunta.length, true);
     });
   }
 
@@ -148,117 +152,115 @@ class _ExerciciosState extends State<Exercicios> {
             fit: BoxFit.cover,
           ),
         ),
-        child: isLoading
-            ? Center(child: CircularProgressIndicator())
-            : palavras.isEmpty
-                ? Center(
-                    child: Text(
-                      "Nenhuma palavra encontrada.",
-                      style: TextStyle(color: Colors.white, fontSize: 20),
+        child: Column(
+          children: [
+            SizedBox(height: 5),
+            //Frase de juntar as sílabas
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Junte as sílabas",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            //Contém os componentes centralizados
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  //Video
+                  Container(
+                    child: Column(
+                      children: [
+                        //Videos
+                        VideoPlayerWidget(
+                          videoPath: 'assets/imagens/videos/_Arara.mp4',
+                        ),
+                      ],
                     ),
-                  )
-                : Column(
+                  ),
+
+                  SizedBox(width: 10),
+                  //Imagem e resposta do usuário
+                  Column(
                     children: [
+                      Imagem('assets/imagens/Arara.png'),
                       SizedBox(height: 5),
+                      //Resposta do Usuário
+                      Container(
+                        width: 150, // Define a largura do container
+                        child: Text(
+                          respostaUsuario,
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          softWrap: true,
+                          maxLines: 2,
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      //Botões de resete e conferência
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Text(
-                            "Junte as sílabas",
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
+                          //Botão Resetar
+                          OutlinedButton(
+                            onPressed: _resetarQuestao,
+                            child: Icon(Icons.refresh, color: Colors.white),
+                            style: OutlinedButton.styleFrom(
+                              backgroundColor: Color.fromRGBO(0, 0, 0, 0.6),
+                            ),
+                          ),
+                          SizedBox(width: 10),
+
+                          //Botão Conferir
+                          OutlinedButton(
+                            onPressed: _conferir,
+                            child: Icon(Icons.check, color: Colors.white),
+                            style: OutlinedButton.styleFrom(
+                              backgroundColor: Color.fromRGBO(0, 0, 0, 0.6),
                             ),
                           ),
                         ],
                       ),
-                      SizedBox(height: 10),
-                      Center(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Container(
-                              child: Column(
-                                children: [
-                                  VideoPlayerWidget(
-                                    videoPath:
-                                        'assets/imagens/videos/_Arara.mp4',
-                                  ),
-                                ],
-                              ),
-                            ),
-                            SizedBox(width: 10),
-                            Column(
-                              children: [
-                                Imagem('assets/imagens/Arara.png'),
-                                SizedBox(height: 5),
-                                Container(
-                                  width: 150,
-                                  child: Text(
-                                    respostaUsuario,
-                                    style: TextStyle(
-                                      fontSize: 20,
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                    softWrap: true,
-                                    maxLines: 2,
-                                    textAlign: TextAlign.center,
-                                  ),
-                                ),
-                                SizedBox(height: 10),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    OutlinedButton(
-                                      onPressed: _resetarQuestao,
-                                      child: Icon(Icons.refresh,
-                                          color: Colors.white),
-                                      style: OutlinedButton.styleFrom(
-                                        backgroundColor:
-                                            Color.fromRGBO(0, 0, 0, 0.6),
-                                      ),
-                                    ),
-                                    SizedBox(width: 10),
-                                    OutlinedButton(
-                                      onPressed: _conferir,
-                                      child: Icon(Icons.check,
-                                          color: Colors.white),
-                                      style: OutlinedButton.styleFrom(
-                                        backgroundColor:
-                                            Color.fromRGBO(0, 0, 0, 0.6),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                            SizedBox(width: 20),
-                            Expanded(
-                              child: Column(
-                                children:
-                                    List.generate(perguntas.length, (index) {
-                                  return Visibility(
-                                    visible: visibilidadeBotoes[index],
-                                    child: BotaoExercicios(
-                                      texto: perguntas[index],
-                                      onPressed: () {
-                                        setState(() {
-                                          _resposta(perguntas[index]);
-                                          visibilidadeBotoes[index] = false;
-                                        });
-                                      },
-                                    ),
-                                  );
-                                }),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
                     ],
                   ),
+                  SizedBox(width: 20),
+
+                  //Botões
+                  Expanded(
+                    child: Column(
+                      children: List.generate(pergunta.length, (index) {
+                        return Visibility(
+                          visible: visibilidadeBotoes[index],
+                          child: BotaoExercicios(
+                            texto: pergunta[index],
+                            onPressed: () {
+                              setState(() {
+                                _resposta(pergunta[index]);
+                                visibilidadeBotoes[index] = false;
+                              });
+                            },
+                          ),
+                        );
+                      }),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
